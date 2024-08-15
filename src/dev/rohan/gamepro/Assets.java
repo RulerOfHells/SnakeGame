64437f8535cba9b4df7dd152a727ab66b2fab06e
Rohan Tripathy <rohntripathy44@gmail.com>
2024-08-15 18:33:55 +0530

    adding pause menu animation

--- src/dev/rohan/gamepro/Assets.java
+++ src/dev/rohan/gamepro/Assets.java
@@ -10,13 +10,15 @@ public class Assets {   //Utility class to provide static resources
         throw new IllegalStateException("Utility class");
     }
     static final BufferedImage[] menu = new BufferedImage[2];
-    static final BufferedImage[] pause = new BufferedImage[2];
+    static final BufferedImage[] pause = new BufferedImage[3];
     static void init() {
         try {
             menu[0] = ImageIO.read(Assets.class.getResourceAsStream("/textures/start1_cpy.png"));
             menu[1] = ImageIO.read(Assets.class.getResourceAsStream("/textures/start2_cpy.png"));
             
             pause[0] = ImageIO.read(Assets.class.getResourceAsStream("/textures/pause_cpy.png"));
+            pause[1] = ImageIO.read(Assets.class.getResourceAsStream("/textures/pause_cpy1.png"));
+            pause[2] = ImageIO.read(Assets.class.getResourceAsStream("/textures/pause_cpy2.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/dev/rohan/gamepro/PauseState.java
+++ src/dev/rohan/gamepro/PauseState.java
@@ -11,16 +11,24 @@ public class PauseState extends State {
         this.handler = handler;
     }
 
-    private boolean isHovering(int x, int y) {               //checks for mouse hover in menu title                
+    private int isHovering(int x, int y) {               //checks for mouse hover in menu title                
         Rectangle rect = new Rectangle(152, 320, 437, 103);
-        return rect.contains(x, y);
+        Rectangle rect2 = new Rectangle(253, 428, 239, 110);
+        if(rect.contains(x, y))
+            return 1;
+        if(rect2.contains(x, y))
+            return 2;
+        return 0;
     }
 
     @Override
     public void tick() {
         if(handler.getMouseManager().isClick() &&
-                isHovering(handler.getMouseManager().getX(), handler.getMouseManager().getY()))
+                isHovering(handler.getMouseManager().getX(), handler.getMouseManager().getY()) == 1)
            State.setState(handler.getGame().getGameState());      //switch state on mouse click
+        else if(handler.getMouseManager().isClick() &&
+                isHovering(handler.getMouseManager().getX(), handler.getMouseManager().getY()) == 2)
+            handler.getGame().stop();
 
         if(handler.getMenuActionManager().isAction())
            handler.getGame().stop();
@@ -29,10 +37,11 @@ public class PauseState extends State {
     @Override
     public void render(Graphics g) {
         if(Assets.menu[0] != null && Assets.menu[1] != null) {      //Menu hover animation
-            if(isHovering(handler.getMouseManager().getX(), handler.getMouseManager().getY()))
-                g.drawImage(Assets.pause[0], 0, 0, null);
-            else
-                g.drawImage(Assets.pause[0], 0, 0, null);
+            if(isHovering(handler.getMouseManager().getX(), handler.getMouseManager().getY()) == 1)
+                g.drawImage(Assets.pause[1], 0, 0, null);
+            else if(isHovering(handler.getMouseManager().getX(), handler.getMouseManager().getY()) == 2)
+                g.drawImage(Assets.pause[2], 0, 0, null);
+            else g.drawImage(Assets.pause[0], 0, 0, null);
         }
     }
 
